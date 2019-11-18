920878faa1192df3fb349747a7473b8a8efdb1b0
ingdotrodriguezbarrios <ing.rodriguezbarrios@gmail.com>
2019-11-17 21:38:41 -0500

    1 file modified

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,22 +3,24 @@ import { User } from './../model/user';
 
 @Injectable()
 export class UserService {
-  dbName : string = "database";
-  objectStorageName : string = "user";
-  user : User[] = [
+  private static DB_NAME : string = "database";
+  private static OBJECT_STORAGE_NAME : string = "user";
+  private static INITIAL_USER_LIST : User[] = [
       {id:1,name:"Jeferson",email:"jr@zyos.co"},
       {id:2,name:"Miguelo",email:"mtorres@zyos.co"},
       {id:2,name:"Sergio",email:"sh@zyos.co"}
     ]; 
   constructor() { 
-    let request : IDBOpenDBRequest = indexedDB.open(this.dbName,1);
+    let request : IDBOpenDBRequest = indexedDB.open(UserService.DB_NAME,1);
     request.onsuccess = this.initializeDatabase;
   }
 
   private initializeDatabase(event:Event){
     let target : IDBOpenDBRequest = <IDBOpenDBRequest>event.target;
     let db : IDBDatabase = target.result;
-    let objectStorage = db.createObjectStore(this.objectStorageName,{keyPath:"id"});
+    let objectStorage = db.createObjectStore(UserService.OBJECT_STORAGE_NAME,{keyPath:"id"});
+    objectStorage.transaction.oncomplete = function(event){
+      db.transaction(UserService.OBJECT_STORAGE_NAME,"readwrite")
+    };
   }
-
 }
