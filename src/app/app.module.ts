ba5ccf56c2a74897f995343fdf7a6ba181a207a5
ingdotrodriguezbarrios <ing.rodriguezbarrios@gmail.com>
2019-11-18 23:54:26 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
 
-import { UserService } from './services/user.service';
-import { UserlistComponent } from './user/userlist/userlist.component';
+import { UserService } from "./services/user.service";
+import { UserlistComponent } from "./user/userlist/userlist.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ UserlistComponent ],
-  bootstrap:    [ UserlistComponent ],
-  providers: [UserService],
-  RouterModule.forRoot([
-      { path: '', component: UserlistComponent }
-    ])
+   imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  declarations: [UserlistComponent],
+  bootstrap: [UserlistComponent],
+  providers: [UserService]
 })
-export class AppModule { }
+export class AppModule {}
