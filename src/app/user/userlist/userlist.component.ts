df1835ee8ae5bc51f583106ab156e62603cb177c
ingdotrodriguezbarrios <ing.rodriguezbarrios@gmail.com>
2019-11-19 00:19:46 -0500

    5 files modified, 1 deleted

--- src/app/user/userlist/userlist.component.ts
+++ src/app/user/userlist/userlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faDelete,faEdit } from '@fortawesome/free-solid-svg-icons';
 import { UserService } from './../../services/user.service';
 import { User } from './../../model/user';
 
